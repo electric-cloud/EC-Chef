c361dc0c6d07cd08d4d83e852719a50bfa30f486
Polina Shubina <925043@gmail.com>
2018-02-08 20:25:57 +0300

    Elevation utility for chef-client

--- src/main/resources/project/drivers/RunChefClientDriver.pl
+++ src/main/resources/project/drivers/RunChefClientDriver.pl
@@ -37,10 +37,10 @@ $|=1;
 # -------------------------------------------------------------------------
 # Main functions
 # -------------------------------------------------------------------------
- 
+
 ###########################################################################
 =head2 main
- 
+
   Title    : main
   Usage    : main();
   Function : Performs a Chef run
@@ -53,7 +53,7 @@ $|=1;
 sub main {
     my $ec = ElectricCommander->new();
     $ec->abortOnError(0);
-    
+
     # -------------------------------------------------------------------------
     # Parameters
     # -------------------------------------------------------------------------
@@ -67,7 +67,8 @@ sub main {
     $::g_interval = ($ec->getProperty( "interval" ))->findvalue('//value')->string_value;
     $::g_log_level = ($ec->getProperty( "log_level" ))->findvalue('//value')->string_value;
     $::g_additional_commands = ($ec->getProperty( "additional_commands" ))->findvalue('//value')->string_value;
-    
+    $::g_elevation_utility = ($ec->getProperty( "elevation_utility" ))->findvalue('//value')->string_value;
+
     #Variable that stores the command to be executed
     $::g_command = $::g_chef_client_path;
 
@@ -80,63 +81,67 @@ sub main {
     my $pluginName = $xpath->findvalue('//pluginVersion')->value;
     print "Using plugin $pluginKey version $pluginName\n";
     print "Running procedure RunChefClient\n";
-    
+
     #Parameters are checked to see which should be included
     if($::g_node_name && $::g_node_name ne '')
     {
         $::g_command = $::g_command . " --node-name " . $::g_node_name;
     }
-    
+
     if($::g_replace_current_run_list && $::g_replace_current_run_list ne '')
     {
         $::g_command = $::g_command . " --override-runlist " . $::g_replace_current_run_list;
     }
-    
+
     if($::g_json_attributes_definition && $::g_json_attributes_definition ne '')
     {
         $::g_command = $::g_command . " --json-attributes " . $::g_json_attributes_definition;
     }
-    
+
     if($::g_chef_server_url && $::g_chef_server_url ne '')
     {
         $::g_command = $::g_command . " --server " . $::g_chef_server_url;
     }
-    
+
     if($::g_configuration_file && $::g_configuration_file ne '')
     {
         $::g_command = $::g_command . " --config " . $::g_configuration_file;
     }
-    
+
     if($::g_interval && $::g_interval ne '')
     {
         $::g_command = $::g_command . " --interval " . $::g_interval;
     }
-    
+
     if($::g_log_level && $::g_log_level ne '')
     {
         $::g_command = $::g_command . " --log_level " . $::g_log_level;
     }
-    
+
     if($::g_log_file_location && $::g_log_file_location ne '')
     {
         $::g_command = $::g_command . " --logfile " . $::g_log_file_location;
     }
-    
+
     if($::g_daemonize && $::g_daemonize ne '')
     {
         $::g_command = $::g_command . " --daemonize";
     }
-    
+
     if($::g_additional_commands && $::g_additional_commands ne '')
     {
         $::g_command = $::g_command . " " . $::g_additional_commands;
     }
 
+    if ($::g_elevation_utility) {
+        $::g_command = $::g_elevation_utility . ' ' . $::g_command;
+    }
+
     #Print out the command to be executed
     print "\nCommand to be executed: \n$::g_command \n\n";
-    
+
     #Executes the command
     system("$::g_command");
 }
-  
+
 main();
